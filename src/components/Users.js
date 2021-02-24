bb026bb652652e4ad49d2e9c140880fe7ff4baa8
sonamk15 <sonamk18@navgurukul.org>
2021-02-24 17:19:27 +0530

    implement pagination

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Modal from './Modal'
+import Pagination from './Pagination';
+
 
 class Users extends Component {
     constructor(props) {
@@ -11,7 +13,9 @@ class Users extends Component {
             loading: false,
             data:{},
             showModal:false,
-            dataModal: {}
+            dataModal: {},
+            perPage: 5,
+            currentPage: 1
         }
     }
 
@@ -78,7 +82,15 @@ class Users extends Component {
     }
 
     render() {
-        var { items, loading, users, flag,data, showModal,dataModal } = this.state
+        var { items, loading, users, data, showModal,dataModal,perPage, currentPage} = this.state
+
+          // Get current posts
+        const indexOfLastPost = currentPage * perPage;
+        const indexOfFirstPost = indexOfLastPost - perPage;
+        const currentPosts = users.slice(indexOfFirstPost, indexOfLastPost);
+        const paginate = pageNumber => this.setState({currentPage:pageNumber});
+
+        // add a new user
         const addUser = ()=>{
             const requestOptions = {
                 method: 'POST',
@@ -103,43 +115,39 @@ class Users extends Component {
                           <div>
                             <button href="#" className="btn btn-danger" onClick={this.removeAllUser} >Clear All</button>
                             <button href="#" className="btn btn-success" onClick={addUser}>Add User</button>
-
                                 <div className="card" style={{width:'30%' ,marginLeft:'30%', marginTop:'20px', border:'1px solid #ccc', padding:'30px'}}>
-                                <img src={item.picture.medium} className="card-img-top" alt={item.name.first}/>
-                                <div className="card-body">
-                                 <h5 className="card-title">Name: {item.name.first}</h5>
-                                 <h5 className="card-title">Phone No: {item.phone}</h5>
+                                    <img src={item.picture.medium} className="card-img-top" alt={item.name.first}/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">Name: {item.name.first}</h5>
+                                        <h5 className="card-title">Phone No: {item.phone}</h5>
+                                    </div>
                                 </div>
-                         
-                          </div>
-          
-                          </div>
-                          
-                                         
+                            </div>
                         ))}                  
                     </div>
                     <h2>Listed Users</h2> 
                     <div className="container">
-                        {users.map((user,index) => (
+                        {currentPosts.map((user,index) => (
                           <div>
-                                <div className='well well-sm' style={{width:'80%', display:'inline-table'}} onClick={() => this.getModal(user)}>
+                              <div className='well well-sm' style={{width:'80%', display:'inline-table'}} onClick={() => this.getModal(user)}>
                                 <img   src={user.image} className="img-rounded" alt={user.name}/>
                                <span style = {{marginLeft:"25px"}}>Name: {user.name}</span>
                                <span style = {{marginLeft:"50px"}}>Phone: {user.phone}</span>
-
-                               {/* <button >Popup</button> */}
+                              </div>
+                              <button style={{padding:'5px', marginLeft:"10px", marginBottom:'20px' , display:'inline',left:'50px'}} onClick={() => { this.removeUser(user.id) }} className="delete-btn">Delete</button>
                           </div>
-                          <button style={{padding:'5px', marginLeft:"10px", marginBottom:'20px' , display:'inline',left:'50px'}} onClick={() => { this.removeUser(user.id) }} className="delete-btn">Delete</button>
-                          </div>
-
-                                                                   
                         ))}  
-                          <Modal
+                        <Modal
                         show={showModal}
                         onHide={this.hideModal}
                         data={dataModal}
                         />                
                     </div>
+                    <Pagination
+                    postsPerPage={perPage}
+                    totalPosts={users.length}
+                    paginate={paginate}
+                />
                 </div>
             )
         }
